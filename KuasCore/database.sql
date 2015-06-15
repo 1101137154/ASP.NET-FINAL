56d05058625181025ac1d756cc303f4c4673990a
1101137154 <mahendraagnidharma@yahoo.co.id>
2015-06-15 17:34:25 +0800

    database fixed

--- KuasCore/database.sql
+++ KuasCore/database.sql
@@ -1,22 +1,30 @@
-﻿
-CREATE DATABASE [KUAS]
+﻿USE [ASP_NET_FINAL]
 GO
 
-USE [KUAS]
-GO
-
-CREATE TABLE [dbo].[Employee](
-	[id]   [nvarchar](50) NOT NULL,
-	[name] [nvarchar](255) NOT NULL,
-	[age]  [int] NULL,
- CONSTRAINT [PK_Employees] PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[Students](
+	[id]   	 [int] IDENTITY(1,1),
+	[stu_id] [nvarchar](50) NOT NULL,
+	[stu_name] [nvarchar](50) NOT NULL,
+	[stu_phone] [nvarchar](50) NOT NULL,
+	[stu_birth] [date] NOT NULL,
+	[stu_info] [nvarchar](1000) NULL
+ CONSTRAINT [PK_Students] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
-INSERT [dbo].[Employee] ([id], [name], [age]) VALUES (N'dennis_yen'  , N'嚴志和', 35);
-INSERT [dbo].[Employee] ([id], [name], [age]) VALUES (N'james_ch_lee', N'李建祥', 40);
-INSERT [dbo].[Employee] ([id], [name], [age]) VALUES (N'sean_huang'  , N'黃以新', 28);
+INSERT INTO [dbo].[Students]
+           ([stu_id]
+           ,[stu_name]
+           ,[stu_phone]
+           ,[stu_birth]
+           ,[stu_info])
+     VALUES
+           (N's1101137114'
+           , N'晨曦'
+           ,'0123456789'
+           ,'1993/10/12'
+           ,N'有太陽般的笑容')
 GO
